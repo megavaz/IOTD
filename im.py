a425230b4b8676a5556a3a2226b3e839ea40e712
megavaz <fruitpizza49@gmail.com>
2020-04-08 13:08:05 +0300

    little adjusment

--- im.py
+++ im.py
@@ -3,6 +3,7 @@ import os
 from datetime import datetime
 from random import randrange
 from datetime import timedelta
+import time
 
 def random_date(start, end):
     """
@@ -18,14 +19,20 @@ def random_date(start, end):
 d1 = datetime.strptime('1/1/2010', '%m/%d/%Y')
 d2 = datetime.now()
 
-a = random_date(d1, d2).strftime("%Y-%m-%d")
-url = ("https://api.nasa.gov/planetary/apod?api_key=Your_api_key="+a)
+rnd = random_date(d1, d2).strftime("%Y-%m-%d")
+api_key = ''
+url = ("https://api.nasa.gov/planetary/apod?api_key=" + api_key + "&date=" + rnd)
 file_path = os.path.dirname(os.path.realpath(__file__)) + r"\image_name.jpg"
 request = requests.get(url).json()
 print(request)
 image_url = request['hdurl']
-request1 = requests.get(image_url)
+try:
+    request1 = requests.get(image_url)
+except:
+    print('Couldn\'t get hd version :(')
+    time.sleep(2)
 picture = request1.content
+
 if os.path.isfile(file_path):
     os.remove(file_path)
 with open(file_path, 'wb') as handler:
